3351e02b40c9d69298f9bac70d04b8a0f2138471
Anoop D'cunha <Anoop.Dcunha@godigit.com>
2022-03-23 21:28:05 +0530

    added authController

---
+++ controllers/authController.js
@@ -0,0 +1,82 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+class AuthController {
+    userData = {};
+    payload = {};
+    user = {};
+
+    static getApiRouteRootPath = (req, res) => {
+        res.status(200).send('Server/API Ack.');
+    }
+
+    static getLogin = (req, res) => {
+        res.status(200).json({message:'GET/ login not implemented'});
+    }
+
+    static postLogin = (req, res) => {
+        this.userData = req.body;
+
+        User.findOne({
+            email: this.userData.email
+        }, (error, user) => {
+            if (error) {
+                console.error(error);
+            }
+            
+            if (!user) {
+                res.status(401).json({
+                    statusCode: 401,
+                    errorMessage: 'Invalid Email!'
+                });
+            } else {
+                bcrypt.compare(this.userData.password, user.password, (error, result) => {
+                    if (!result) {
+                        res.status(401).json({
+                            statusCode: 401,
+                            errorMessage: 'Invalid Password!'
+                        });
+                    } else {
+                        let payload = {
+                            _id: user._id,
+                            email: user.email,
+                            name: user.name,
+                            profileImageUrl: user.profileImageUrl,
+                            role: user.role
+                        };
+                        let token = jwt.sign(payload, process.env.SECRET_KEY);
+                        res.status(200).json(token);
+                    }
+                });
+            }
+                
+                
+        });
+    }
+
+    static getRegister = (req, res) => {
+        res.status(200).json({message:'GET/ register not implemented'});
+    }
+
+    static postRegister = (req, res) => {
+        this.userData = req.body;
+    
+        bcrypt.hash(this.userData.password, parseInt(process.env.SALT_ROUNDS), (err, hash) => {
+            this.userData.password = hash;
+            this.user = new User(this.userData);
+            this.user.save((error, registeredUser) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    res.status(200).send(registeredUser);
+                }
+            });
+        });
+    
+    }
+}
+
+
+
+module.exports = AuthController;
--- routes/api.js
+++ routes/api.js
@@ -1,52 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
 const mongoose = require('mongoose');
 const verifyToken = require('../middleware/verifyToken');
+const AuthController = require('../controllers/authController');
 
 mongoose.connect(process.env.DB_URI, (error) => {
     if (error) {
-        console.error('Database: ',error);
+        console.error('Database: ', error);
     } else {
         console.log('DB connection successful');
         console.log(process.env.DB_URI);
     }
 });
 
-router.get('/', (req, res) => {
-    res.status(200).send('Server/API Ack.');
-});
+router.get('/', AuthController.getApiRouteRootPath);
 
-router.post('/register', (req, res) => {
-    let userData = req.body;
-    let user = new User(userData);
-    user.save((error, registeredUser)=>{
-        if (error) {
-            console.error(error);
-        } else {
-            res.status(200).send(registeredUser);
-        }
-    });
-});
+router.get('/register', AuthController.getRegister);
 
-router.post('/login', (req, res) => {
-    let userData = req.body;
-    User.findOne({email: userData.email}, (error, user) => {
-        if (error) {
-            console.error(error);
-        } else {
-            if (!user) {
-                res.status(401).json({statusCode: 401, errorMessage: 'Invalid Email!'});
-            } else if(user.password !== userData.password){
-                res.status(401).json({statusCode: 401, errorMessage: 'Invalid Password!'});
-            } else {
-                let payload =  { _id: user._id, email: user.email, name: user.name, profileImageUrl: user.profileImageUrl,role: user.role };
-                let token = jwt.sign(payload, process.env.SECRET_KEY);
-                res.status(200).json(token);
-            }
-        }
-    });
-});
+router.get('/login', AuthController.getLogin);
+
+router.post('/register', AuthController.postRegister);
+
+router.post('/login', AuthController.postLogin);
 
 module.exports = router;
